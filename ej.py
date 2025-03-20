f2415c1dad44bca3f412b10f545457726827959c
milennne <milene.fuentes@tecsup.edu.pe>
2025-03-20 10:39:17 -0500

    actualizando los archivos .py

--- ej.py
+++ ej.py
@@ -1,1 +1,12 @@
-print("hhh")
+from timeit import default_timer  as timer
+
+n=10**3
+start=timer()
+for i in range (0,n):
+    for j in range (0,n):
+        print(f"i={i} and j={j}")
+        #pass
+
+end=timer()
+proc_time=end-start
+print(f"Processing time -> {proc_time}")
