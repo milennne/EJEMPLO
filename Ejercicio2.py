7aff7c142281b40273b39aa639c9a770f64e7300
bryan <coronelbryan900@gmail.com>
2025-03-20 11:09:49 -0500

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -4,35 +4,31 @@
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 
-
 n_values = [10**2, 10**3, 10**4, 10**5, 10**6]
 
-
 processing_times = []
-
 
 for n in n_values:
     start = timer()  
 
-    for i in range(0, n):  # Bucle simple
-        pass  # Aca se elimina la impresion para que sea mas preciso al medir el tiempo
+    for i in range(0, n):  # Simple loop
+        pass  # The print is removed to make the measurement more accurate
 
     end = timer()  
 
     proc_time = end - start  
     processing_times.append(proc_time)  
 
-
     print(f"Processing time for n = {n} -> {proc_time} seconds")
 
-
 plt.figure(figsize=(10, 6))
-plt.plot(n_values, processing_times, marker='o', linestyle='-', color='blue', label='Tiempo de procesamiento')
-plt.title('Tiempo de procesamiento vs. Tamaño de entrada (n)')
-plt.xlabel('Tamaño de entrada (n)')
-plt.ylabel('Tiempo de procesamiento (segundos)')
+plt.plot(n_values, processing_times, marker='o', linestyle='-', color='blue', label='Processing time')
+plt.title('Processing Time vs. Input Size (n)')
+plt.xlabel('Input Size (n)')
+plt.ylabel('Processing Time (seconds)')
 plt.xscale('log') 
 plt.yscale('log')  
 plt.grid(True)
 plt.legend()
 plt.show()
+
