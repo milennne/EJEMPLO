1bc97fb33ddcf0b34e0211cec3edfe6c91e7fdb0
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-04-24 10:38:00 -0500

    Update Ejercicio1.py

--- LAB06/Ejercicio1.py
+++ LAB06/Ejercicio1.py
@@ -1,21 +1,15 @@
 
 def sliding_window_maximum(nums, k):
-    #SI LA LISTA ESTA VACIA O K ES MENOR O IGUAL A 0 O K ES MAYOR QUE LA LONGITUD DE LA LISTA, RETORNA UNA LISTA VACIA
     if not nums or k <= 0 or k > len(nums):
-        return [] #NO HAY NINGUN "MAXIMO"/VENTANA QUE RETORNAR
+        return []
 
-    result = [] #AQUI VAMOS A GUARDAR LOS MÁXIMO DE CADA VENTANA
-    deque = [] #AQUI VAMOS A GUARDAR LOS ÍNDICES DE LOS ELEMENTOS EN LA VENTANA (NO LOS VALORES)
-    #SON LOS ELEMENTOS CANDIDATOS QUE ESTAN EN LA VENTANA QUE PUEDEN SER EL MÁXIMO.
-    for i in range(len(nums)): #RECORRE CADA INDICE DE LA LISLA NUMS.
-        
-        #MIENTRAS EL DEQUE NO ESTA VACIO Y EL PRIMER INDICE GUARDADO EN EL DEQUE ESTÉ AFUERA DE LA VENTANA ACTUAL, ELIMINA EL PRIMER ÍNDICE DEL DEQUE.
+    result = []
+    deque = []
+    for i in range(len(nums)):
         while deque and deque[0] < i - k + 1:
-            deque.pop(0) #SE ENCARGA DE ELIMINAR LOS ÍNDICES QUE ESTAN FUERA DE LA VENTANA (QUE NO SON CANDIDATOS A SER MÁXIMO)
-        #    
+            deque.pop(0)
         while deque and nums[deque[-1]] < nums[i]:
             deque.pop()
-        #AGREGA EL INDICE ACTUAL AL DEQUE
         deque.append(i)
         if i >= k - 1:
             result.append(nums[deque[0]])
